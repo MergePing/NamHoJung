df81025575e3e749f01d0cc48b85f4db38ec0927
youngseok-1 <whdudtjr0@gmail.com>
2024-10-30 15:32:33 +0900

    frontadminpage

--- src/main/java/com/ohgiraffers/mergyping/comment/model/dto/user/controller/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.ohgiraffers.mergyping.comment.model.dto.user.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class UserController {
-}
--- src/main/java/com/ohgiraffers/mergyping/notice/controller/NoticeController.java
+++
@@ -1,7 +0,0 @@
-package com.ohgiraffers.mergyping.notice.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class NoticeController {
-}
