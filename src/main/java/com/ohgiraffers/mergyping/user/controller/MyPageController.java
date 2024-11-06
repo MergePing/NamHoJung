6370391a364a2c044cc1c6aa82013816725ce685
황순빈 <ghkdtnsqls04@gmail.com>
2024-11-06 09:16:30 +0900

    feature:Mypage-Password-Confirm/2024-11-06

--- src/main/java/com/ohgiraffers/mergyping/user/controller/MyPageController.java
+++ src/main/java/com/ohgiraffers/mergyping/user/controller/MyPageController.java
@@ -5,15 +5,14 @@ import com.ohgiraffers.mergyping.user.model.dto.MyPageDTO;
 import com.ohgiraffers.mergyping.user.model.dto.MyPagePostDTO;
 import com.ohgiraffers.mergyping.user.model.service.MyPageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -23,10 +22,12 @@ import java.util.Map;
 public class MyPageController {
 
     private final MyPageService myPageService;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public MyPageController(MyPageService myPageService) {
+    public MyPageController(MyPageService myPageService, PasswordEncoder passwordEncoder) {
         this.myPageService = myPageService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/userinfo")
@@ -55,22 +56,29 @@ public class MyPageController {
     }
 
     @PostMapping("/userinfo")
-    public String modifyUserName(@ModelAttribute MyPageDTO myPageDTO) {
+    public String modifyUserName(@ModelAttribute MyPageDTO myPageDTO,@RequestParam(required = false) String newPassword) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication != null && authentication.getPrincipal() instanceof AuthDetails) {
             AuthDetails userDetails = (AuthDetails) authentication.getPrincipal();
             int userNo = userDetails.getLoginUserDTO().getUserNo();  // 로그인한 사용자의 userNo 가져오기
-
             // MyPageDTO에 로그인된 사용자 userNo 설정
             myPageDTO.setUserNo(userNo);
             myPageService.modifyUserName(myPageDTO);
+
+            // 비밀번호가 전달된 경우 비밀번호 업데이트
+            if (newPassword != null && !newPassword.isEmpty()) {
+                String encodedPassword = passwordEncoder.encode(newPassword);  // 비밀번호 암호화
+                myPageService.updatePassword(userNo, encodedPassword);  // 암호화된 비밀번호로 업데이트
+            }
         } else {
             return "redirect:/login";  // 인증되지 않은 경우 로그인 페이지로 리다이렉트
         }
 
         return "redirect:/userinfo";
     }
+
+
 
     @GetMapping("/useractive")
     public String findNickName(Model model) {
