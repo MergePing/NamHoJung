9d440e369fc1a4e5b58e2439e9ba8ffb93aa6232
Pear1yCode <gh_wns@naver.com>
2024-11-08 11:25:29 +0900

    feature:MbtiTest/2024-11-08

--- src/main/java/com/ohgiraffers/mergyping/mbti/controller/MbtiController.java
+++ src/main/java/com/ohgiraffers/mergyping/mbti/controller/MbtiController.java
@@ -2,11 +2,13 @@ package com.ohgiraffers.mergyping.mbti.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/mbti")
 public class MbtiController {
 
-    @GetMapping("/mbti")
+    @GetMapping()
     public String mbtiTest() {
 
         return "/mbti/mbtistart";
