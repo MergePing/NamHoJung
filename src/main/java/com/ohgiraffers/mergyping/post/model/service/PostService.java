565a6ac144c5dfd175515b9c93e757e163c49a49
manbeao <lesles0906@naver.com>
2024-11-09 01:13:48 +0900

    feat:postService/2024-11-09

--- src/main/java/com/ohgiraffers/mergyping/post/model/service/PostService.java
+++ src/main/java/com/ohgiraffers/mergyping/post/model/service/PostService.java
@@ -13,14 +13,18 @@ import java.util.List;
 @Service
 public class PostService {
 
+    // 매퍼 주입
     private final PostMapper postMapper;
-    private JdbcTemplate jdbcTemplate;
 
+
+    // 매퍼 주입을 위한 생성자
     @Autowired
     public PostService(PostMapper postMapper) {
         this.postMapper = postMapper;
     }
 
+
+    // 게시글 목록 조회하는 메소드
     public List<PostDTO> postList() {
         List<PostDTO> postList = postMapper.postList();
 
@@ -28,60 +32,99 @@ public class PostService {
     }
 
 
-    //즐겨찾기 상태
+    //즐겨찾기 상태 업데이트
     public void updateFavoriteStatus(int postNo, boolean isFavorite) {
+
+        // 게시글 번호와 즐겨찾기 상태룰 매퍼를 통해 업데이트
         postMapper.updateFavoriteStatus(postNo, isFavorite);
     }
 
 
-    // 세부 게시글 조회
+    // 게시글 번호로 세부 게시글 조회
     public SelectPostDTO selectById(int postNo) {
+
+        // 게시글 번호를 통해 매퍼로 특정 게시물 조회, 반환
         return postMapper.selectById(postNo);
     }
 
 
     // 게시글 목록 조회
     public List<PostDTO> getPostList() {
+
+        // 매퍼를 통해 게시물 전체 목록 반환
         return postMapper.postList();
     }
 
 
-    //무서워요 상태
+    //무서워요 상태 업데이트
     public void updateScaryStatus(int postNo, boolean isScary) {
+
+        // 무서워요 상태가 true인 경우 무서워요 수 증가
         if (isScary) {
             postMapper.incrementScaryCount(postNo, true);
-        } else {
+        }
+
+        // 무서워요 상태가 false인 경우 무서워요 수 감소
+        else {
             postMapper.decrementScaryCount(postNo, false);
         }
     }
 
+
+    // 안무서워요 상태 업데이트
     public void updateNotScaryStatus(int postNo, boolean isNotScary) {
+
+        // 안무서워요 상태가 true인 경우 안무서워요 수 증가
         if (isNotScary) {
             postMapper.incrementNotScaryCount(postNo, true);
-        } else {
+        }
+
+        // 안무서워요 상태가 false인 경우 안무서워요 수 감소
+        else {
             postMapper.decrementNotScaryCount(postNo, false);
         }
     }
 
-    // 무서워요 숫자
+
+    // 무서워요 숫자 조회
     public int getScaryNumber(int postNo) {
+
+        // 게시물 번호를 통해 매퍼로 특정 게시물의 무서워요 수 조회 후 반환
         return postMapper.getScaryNumber(postNo);
     }
 
+
+    // 안무서워요 숫자 조회
     public int getNotScaryNumber(int postNo) {
+
+        // 게시물 번호를 통해 매퍼로 특정 게시물의 안무서워요 수 조회 후 반환
+
         return postMapper.getNotScaryNumber(postNo);
     }
 
 
+    // 새로운 게시물 생성
     public void createPost(SelectPostDTO selectPostDTO) {
+
+        // 컨트롤러에서 만들어 놓은 selectPostDTO 를 매퍼를 통해 삽입
         postMapper.insertPost(selectPostDTO);
     }
 
+
+    // 게시물 개수 조회
     public int getPostCount() {
+
+        // 매퍼를 통해 게시물의 개수 반환
         return postMapper.getPostCount();
     }
 
+
+    // 최대 게시물 번호 조회
     public int getMaxPostNo() {
+
+        // 매퍼를 통해 게시물 번호 중에 가장 큰 값 반환
         return postMapper.getMaxPostNo();
     }
+
+
 }
