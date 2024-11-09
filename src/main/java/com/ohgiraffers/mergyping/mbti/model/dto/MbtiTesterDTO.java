49c68d62a2a67562f947f3ccf04c22d1b7013d59
Pear1yCode <gh_wns@naver.com>
2024-11-09 22:39:58 +0900

    feature:MbtiTest/2024-11-09

--- src/main/java/com/ohgiraffers/mergyping/mbti/model/dto/MbtiTesterDTO.java
+++ src/main/java/com/ohgiraffers/mergyping/mbti/model/dto/MbtiTesterDTO.java
@@ -59,4 +59,34 @@ public class MbtiTesterDTO {
                 ", TM=" + TM +
                 '}';
     }
+
+    // 이건 혁신이다. 알수록 무궁무진한 코딩의 세계
+    public void updateMbti(int questionNo, String answer) {
+        if (questionNo >= 1 && questionNo <= 9) { // CB 카테고리 (1~9번)
+            if ("yes".equals(answer)) {
+                CB++; // 예일 때 증가
+            } else if ("no".equals(answer)) {
+                CB--; // 아니오일 때 감소
+            }
+        } else if (questionNo >= 10 && questionNo <= 18) { // HG 카테고리 (10~18번)
+            if ("yes".equals(answer)) {
+                HG++;
+            } else if ("no".equals(answer)) {
+                HG--;
+            }
+        } else if (questionNo >= 19 && questionNo <= 27) { // SE 카테고리 (19~27번)
+            if ("yes".equals(answer)) {
+                SE++;
+            } else if ("no".equals(answer)) {
+                SE--;
+            }
+        } else if (questionNo >= 28 && questionNo <= 36) { // TM 카테고리 (28~36번)
+            if ("yes".equals(answer)) {
+                TM++;
+            } else if ("no".equals(answer)) {
+                TM--;
+            }
+        }
+    }
+    
 }
