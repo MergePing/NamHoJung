4c4d10e61af66d76162cfda8522d9369af5ea2d6
Pear1yCode <gh_wns@naver.com>
2024-11-10 21:33:40 +0900

    feature:MbtiTest/2024-11-10

--- src/main/java/com/ohgiraffers/mergyping/mbti/controller/MbtiController.java
+++ src/main/java/com/ohgiraffers/mergyping/mbti/controller/MbtiController.java
@@ -42,10 +42,8 @@ public class MbtiController {
         return mbtiService.getQuestionByNo(questionNo);
     }
 
-    // 결과 페이지 요청
     @GetMapping("/mbtiresult")
     public String resultPage(Model model) {
-        // 해당 유저의 MBTI 정보를 DB에서 가져와서 model에 담기
         model.addAttribute("message", "MBTI 결과가 성공적으로 업데이트되었습니다.");
         return "/mbti/mbtiresult";
     }
@@ -53,23 +51,18 @@ public class MbtiController {
     @PostMapping("/submitmbtiresult")
     @ResponseBody
     public Map<String, String> submitMbtiResult(@RequestBody MbtiTesterDTO mbtiTesterDTO, Authentication authentication) {
-        // 현재 로그인한 사용자의 정보 가져오기
         AuthDetails authDetails = (AuthDetails) authentication.getPrincipal();
-        int userNo = authDetails.getUserNo(); // 로그인한 사용자의 userNo를 얻어옵니다.
+        int userNo = authDetails.getUserNo();
 
-        // MBTI 계산 및 상태 처리
-        String mbtiType = mbtiService.calculateMbti(mbtiTesterDTO);  // MBTI 계산
+        String mbtiType = mbtiService.calculateMbti(mbtiTesterDTO);
 
-        // MBTI 결과를 담을 DTO 생성
         MbtiResultDTO mbtiResultDTO = new MbtiResultDTO();
         mbtiResultDTO.setUserNo(userNo);
         mbtiResultDTO.setMbtiType(mbtiType);
-        mbtiResultDTO.setMbtiStatus(true);  // MBTI 완료 상태를 true로 설정
+        mbtiResultDTO.setMbtiStatus(true);
 
-        // MBTI 결과 업데이트
         mbtiService.updateMbti(userNo, mbtiResultDTO);
 
-        // 성공적으로 처리 후 결과 페이지로 이동
         Map<String, String> response = new HashMap<>();
         response.put("status", "success");
         return response;
--- src/main/java/com/ohgiraffers/mergyping/mbti/model/service/MbtiService.java
+++ src/main/java/com/ohgiraffers/mergyping/mbti/model/service/MbtiService.java
@@ -21,12 +21,9 @@ public class MbtiService {
             return mbtiMapper.getQuestion(questionNo);
     }
 
-    // MBTI 유형 계산
     public String calculateMbti(MbtiTesterDTO mbtiTesterDTO) {
-        // MBTI 유형을 결정할 변수
         String mbtiType = "";
 
-        // 각 점수에 따라 MBTI 유형을 계산
         mbtiType += mbtiTesterDTO.getCB() >= 1 ? "C" : "B";
         mbtiType += mbtiTesterDTO.getHG() >= 1 ? "H" : "G";
         mbtiType += mbtiTesterDTO.getSE() >= 1 ? "S" : "E";
@@ -35,9 +32,7 @@ public class MbtiService {
         return mbtiType;
     }
 
-    // MBTI 유형을 DB에 업데이트
     public void updateMbti(int userNo, MbtiResultDTO mbtiResultDTO) {
-        // mbtiResultDTO 객체를 사용하여 DB에 업데이트
         mbtiMapper.updateMbti(userNo, mbtiResultDTO);
     }
 }
