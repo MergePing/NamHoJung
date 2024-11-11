ebf75db2d8a18023a8c4c65e98bd57b762471eba
manbeao <lesles0906@naver.com>
2024-11-11 16:13:01 +0900

    create:PostSort/2024-11-11

--- src/main/java/com/ohgiraffers/mergyping/post/model/dao/PostMapper.java
+++ src/main/java/com/ohgiraffers/mergyping/post/model/dao/PostMapper.java
@@ -8,12 +8,16 @@ import org.apache.ibatis.annotations.Select;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface PostMapper {
 
     // 게시글 전체 목록조회
     List<PostDTO> postList();
+
+    // 게시글 정렬
+    List<PostDTO> postListSort(@RequestParam("params") Map<String, Object> params);
 
     // 게시글 번호와 즐겨찾기 여부로 즐겨 찾기 상태 업데이트
     void updateFavoriteStatus(@RequestParam("postNo") int postNo, @RequestParam("isFavorite") boolean isFavorite);
--- src/main/java/com/ohgiraffers/mergyping/post/model/service/PostService.java
+++ src/main/java/com/ohgiraffers/mergyping/post/model/service/PostService.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class PostService {
@@ -31,6 +33,15 @@ public class PostService {
         List<PostDTO> postList = postMapper.postList();
 
         return postList;
+    }
+
+    // 게시글 정렬하는 메소드
+    public List<PostDTO> postListSort(String orderBy, String category) {
+        Map<String,Object> params = new HashMap<>();
+        params.put("orderBy",orderBy);
+        params.put("category",category);
+
+        return postMapper.postListSort(params);
     }
 
 
