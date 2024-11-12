dcfed67526c2bb31c5104c26dc2fb1313221dd8d
Pear1yCode <gh_wns@naver.com>
2024-11-12 15:20:00 +0900

    feature:FindAccountId/2024-11-12

--- src/main/java/com/ohgiraffers/mergyping/user/controller/FindController.java
+++ src/main/java/com/ohgiraffers/mergyping/user/controller/FindController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/search")
+@RequestMapping("/find")
 @Controller
-public class SearchController {
+public class FindController {
 
     @GetMapping("/select")
     public String search() {
