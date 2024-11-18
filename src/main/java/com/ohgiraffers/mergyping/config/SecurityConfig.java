f32305c7061fc6a571b4caac62c7d27835342a2c
Pear1yCode <gh_wns@naver.com>
2024-11-18 09:46:12 +0900

    feature:LevelInfo/2024-11-18

--- src/main/java/com/ohgiraffers/mergyping/config/SecurityConfig.java
+++ src/main/java/com/ohgiraffers/mergyping/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig  {
             auth.requestMatchers( "/auth/**",
                     "/", "/main", "/css/**", "/img/**", "/error/**",
                     "/userinfo/**", "/useractive/**", "/intro", "/notice/*", "/selectnotice", "/selectpost","/writepost", "/find/**",
-                    "/checknickname/**", "/admin/**", "/toggleFavorite", "/post/**","selectpost/**","/toggleScary/**","/toggleNotScary/**").permitAll();
+                    "/checknickname/**", "/admin/**", "/toggleFavorite", "/post/**","selectpost/**","/toggleScary/**","/toggleNotScary/**", "/level").permitAll();
             auth.requestMatchers("/admin/*").hasAnyAuthority(UserRole.ADMIN.getRole());
             auth.requestMatchers("/user/*").hasAnyAuthority(UserRole.USER.getRole());
             auth.anyRequest().authenticated();
