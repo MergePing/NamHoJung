7962e42cf58ac520490c8115ef0c4b30e30f9041
Pear1yCode <gh_wns@naver.com>
2024-11-23 21:34:39 +0900

    fix:FixAll/2024-11-19

--- src/main/java/com/ohgiraffers/mergyping/user/controller/CustomErrorController.java
+++ src/main/java/com/ohgiraffers/mergyping/user/controller/CustomErrorController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CustomErrorController implements ErrorController {
-/*    @RequestMapping("/error")
+    @RequestMapping("/error")
     public String handleError(Model model) {
         model.addAttribute("errorMessage", "Something went wrong!");
         return "user/error/server";  // 에러 페이지 뷰 이름
-    }*/
+    }
 }
