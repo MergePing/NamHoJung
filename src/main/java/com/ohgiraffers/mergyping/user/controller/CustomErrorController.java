1022d6c807f8a04bc9a18344471e7dc09992122a
Pear1yCode <gh_wns@naver.com>
2024-11-25 22:31:26 +0900

    fix:FixAll/2024-11-25

--- src/main/java/com/ohgiraffers/mergyping/user/controller/CustomErrorController.java
+++ src/main/java/com/ohgiraffers/mergyping/user/controller/CustomErrorController.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.mergyping.user.controller;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,8 +10,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomErrorController implements ErrorController {
     @RequestMapping("/error")
-    public String handleError(Model model) {
-        model.addAttribute("errorMessage", "Something went wrong!");
-        return "user/error/server";  // 에러 페이지 뷰 이름
+    public String handleError(HttpServletRequest request, Model model) {
+
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == 404) {
+                model.addAttribute("errorMessage", "Page not found");
+                return "user/error/permission";
+            } else if (statusCode == 500) {
+                model.addAttribute("errorMessage", "Internal server error");
+                return "user/error/server";
+            }
+        }
+        model.addAttribute("errorMessage", "Unknown error occurred");
+        return "user/error/page";
     }
 }
